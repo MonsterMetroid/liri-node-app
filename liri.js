3bda4ccda41730c372454d04d482a1f1603218cd
MonsterMetroid <monstermetroid@gmail.com>
2017-06-29 18:02:51 -0700

    integrating twitter api logic pushing to debug error

--- liri.js
+++ liri.js
@@ -4,42 +4,50 @@ var spot = require("node-spotify-api");
 
 var input = process.argv.slice(2);
 
-console.log(tweetKeys.consumer_key);
+var client = new tweet({
+  consumer_key: tweetKeys.twitterKeys.consumer_key,
+  consumer_secret: tweetKeys.twitterKeys.consumer_secret,
+  access_token_key: tweetKeys.twitterKeys.access_token_key,
+  access_token_secret: tweetKeys.twitterKeys.access_token_secret
+});
 
-// function giveCmd(cmd){
-// 	 switch (cmd[0]){
-// 		case `my-tweets`:
-// 			fs.readFile('bank.txt', 'utf8', (err, data) => {
-// 			  if (err){
-// 			  	console.log("error!")
-// 			  }  
-// 			  	console.log(data); 
-// 			});
-// 			break;
-// 		case `spotify-this-song`:
-// 			var song = input[1];
-// 			if (song === null){
-// 				song = "The Sign";
-// 			}
+//console.log(tweetKeys.twitterKeys.consumer_key);
 
-// 			break;
-// 		case `movie-this`:
-// 			fs.readFile('bank.txt', 'utf8', (err, data) => {
-// 			  if (err){
-// 			  	console.log("error!")
-// 			  }  
-// 			  	console.log(data); 
-// 			});
-// 			break;
-// 		case `do-what-it-says`:
-// 			fs.readFile('random.txt', 'utf8', (err, data) => {
-// 			  if (err){
-// 			  	console.log("error!")
-// 			  }  
-// 			  	giveCmd(data); 
-// 			});
-// 			break;
-// 	}
-// }
+function giveCmd(cmd){
+	 switch (cmd){
+		case `my-tweets`:
+			console.log("we in");
+			client.get('favorites/list', function(error, tweets, response) {
+				  if(error) throw error;
+				  console.log(tweets);  // The favorites.
+				  console.log(response);  // Raw response object.
+				});
 
-// giveCmd(input[0]);
+			break;
+		case `spotify-this-song`:
+			var song = input[1];
+			if (song === null){
+				song = "The Sign";
+			}
+
+			break;
+		case `movie-this`:
+			fs.readFile('bank.txt', 'utf8', (err, data) => {
+			  if (err){
+			  	console.log("error!")
+			  }  
+			  	console.log(data); 
+			});
+			break;
+		case `do-what-it-says`:
+			fs.readFile('random.txt', 'utf8', (err, data) => {
+			  if (err){
+			  	console.log("error!")
+			  }  
+			  	giveCmd(data); 
+			});
+			break;
+	}
+}
+
+giveCmd(input[0]);
